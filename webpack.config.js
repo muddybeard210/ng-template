0ebef2dd4935d17d37c48601745719b4a320b5f6
muddybeard210 <mattsmith203@hotmail.com>
2016-08-24 18:41:05 -0700

    basic layout

--- webpack.config.js
+++ webpack.config.js
@@ -64,7 +64,7 @@ module.exports = {
         loader: 'html',
       },
       {
-        test: /\.(jpg|gif|png)$/,
+        test: /\.(jpg|jpeg|gif|png)$/,
         loader: 'file?name=img/[hash].[ext]',
       },
       {
@@ -75,13 +75,13 @@ module.exports = {
         test: /\.woff.*/,
         loader: 'file?name=fonts/[name].[ext]',
       },
-      { 
+      {
         test: /\.[ot]tf.*/,
-        loader: 'url?limit=10000&mimetype=application/octet-stream&name=fonts/[name].[ext]' 
+        loader: 'url?limit=10000&mimetype=application/octet-stream&name=fonts/[name].[ext]'
       },
       {
-        test: /\.eot.*/, 
-        loader: 'url?limit=10000&mimetype=application/vnd.ms-fontobject&name=fonts/[name].[ext]' 
+        test: /\.eot.*/,
+        loader: 'url?limit=10000&mimetype=application/vnd.ms-fontobject&name=fonts/[name].[ext]'
       }
     ],
   },
